bbe4048665ac836a93119bf4db1dcf4305106d75
Thiago Lagden <lagden@gmail.com>
2012-11-23 02:05:36 -0200

    alterado para jade

---
+++ public/scripts/main.js
@@ -0,0 +1,13 @@
+var global;
+
+// jQuery document ready
+jQuery.fn.ready(function(){
+
+
+    // jQuery as $
+    (function($){
+
+        // do some stuff
+        
+    })(jQuery);
+});
--- web.js
+++ web.js
@@ -7,10 +7,9 @@ var async = require('async')
 var app = module.exports = express();
 
 // engine
-// app.engine('.html', require('ejs').__express);
-app.engine('.html', require('ejs-locals'));
+app.engine('jade', require('jade').__express);
+app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
 
 // Config
 app.configure(function(){
@@ -38,66 +37,14 @@ app.configure(function(){
 });
 
 app.set('appName','Movimento Respirar - Controlar');
+app.set('appDescription','Descubra o que aconteceu no dia em que você nasceu.');
 app.set('title','Movimento Respirar - Controlar');
 
-function render_page(req, res){
-    req.facebook.app(function(app) {
-        req.facebook.me(function(user) {
-            res.render('index', {
-                layout:    false,
-                req:       req,
-                app:       app,
-                user:      user
-            });
-        });
-    });
-}
+// Facebook Methods
+var fb = require('./lib/fb');
 
-function handle_facebook_request(req, res, next) {
-
-  // if the user is logged in
-  if (req.facebook.token) {
-
-    async.parallel([
-      function(cb) {
-        // query 4 friends and send them to the socket for this socket id
-        req.facebook.get('/me/friends', { limit: 4 }, function(friends) {
-          req.friends = friends;
-          cb();
-        });
-      },
-      function(cb) {
-        // query 16 photos and send them to the socket for this socket id
-        req.facebook.get('/me/photos', { limit: 16 }, function(photos) {
-          req.photos = photos;
-          cb();
-        });
-      },
-      function(cb) {
-        // query 4 likes and send them to the socket for this socket id
-        req.facebook.get('/me/likes', { limit: 4 }, function(likes) {
-          req.likes = likes;
-          cb();
-        });
-      },
-      function(cb) {
-        // use fql to get a list of my friends that are using this app
-        req.facebook.fql('SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1', function(result) {
-          req.friends_using_app = result;
-          cb();
-        });
-      }
-    ], function() {
-      render_page(req, res);
-    });
-
-  } else {
-    render_page(req, res);
-  }
-}
-
-app.get('/', handle_facebook_request);
-app.post('/', handle_facebook_request);
+app.get('/', fb.methods.handle_facebook_request);
+app.post('/', fb.methods.handle_facebook_request);
 
 
 // listen to the PORT given to us in the environment
